973247e444daca2f535c3f867aa30a3565ba5a9e
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-05-22 21:47:04 -0700

    spaces as tabs? NOT IN MY PROJECT

--- sizes.c
+++ sizes.c
@@ -9,6 +9,6 @@ int main()
 	printf("gcm_ctx: %d\n", sizeof(struct gcm_ctx));
 	printf("gcm_key: %d\n", sizeof(struct gcm_key));
 	printf("gcm_aes_ctx: %d\n", sizeof(struct gcm_aes_ctx));
-    printf("yarrow256_ctx: %d\n", sizeof(struct yarrow256_ctx));
+	printf("yarrow256_ctx: %d\n", sizeof(struct yarrow256_ctx));
 }
 
