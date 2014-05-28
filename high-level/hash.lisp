1ad4297dbd95ef8ca1bacfe3a852ad8920f9ead5
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-05-28 01:09:54 -0700

    updating hashers to use with-crypto-object (surprised we didnt get segfaults here...) and building new hmac authed hash functions. they work great.

--- high-level/hash.lisp
+++ high-level/hash.lisp
@@ -6,16 +6,34 @@
    done."
   (flet ((import-sym (suffix)
            (intern (format nil "~a-~a" hash-type (string suffix)) :nettle)))
-    (let ((ctx-struct (import-sym 'ctx))
-          (init-fn (import-sym 'init))
+    (let ((ctx-struct (intern (string hash-type) :keyword))
           (digest-size (symbol-value (intern (format nil "+~a-DIGEST-SIZE+" hash-type) :nettle)))
+          (init-fn (import-sym 'init))
           (update-fn (import-sym 'update))
           (digest-fn (import-sym 'digest)))
-      (cffi:with-foreign-object (ctx `(:pointer (:struct ,ctx-struct)))
+      (with-crypto-object (ctx ctx-struct)
         (with-static-vectors ((input-s input)
                               (digest-s digest-size))
           (funcall init-fn ctx)
           (funcall update-fn ctx (length input) (static-vector-pointer input-s))
+          (funcall digest-fn ctx digest-size (static-vector-pointer digest-s))
+          (copy-seq digest-s))))))
+
+(defun run-auth-hasher (hash-type key input)
+  "Run an HMAC hasher against the given key."
+  (flet ((import-sym (suffix)
+           (intern (format nil "HMAC-~a-~a" hash-type (string suffix)) :nettle)))
+    (let ((ctx-struct (intern (format nil "HMAC-~a" hash-type) :keyword))
+          (digest-size (symbol-value (intern (format nil "+~a-DIGEST-SIZE+" hash-type) :nettle)))
+          (init-fn (import-sym 'set-key))
+          (update-fn (import-sym 'update))
+          (digest-fn (import-sym 'digest)))
+      (with-crypto-object (ctx ctx-struct)
+        (with-static-vectors ((input-s input)
+                              (key-s key)
+                              (digest-s digest-size))
+          (funcall init-fn ctx (length key-s) (static-vector-pointer key-s))
+          (funcall update-fn ctx (length input) (static-vector-pointer input))
           (funcall digest-fn ctx digest-size (static-vector-pointer digest-s))
           (copy-seq digest-s))))))
 
@@ -25,8 +43,19 @@
      ,(format nil "~a the input vector (octet vectors only, please)." hash-type)
      (run-hasher ',hash-type input)))
     
+(defmacro make-auth-hasher (hash-type)
+  "Defines an HMAC hasher function for us. Simple wrapper around run-auth-hasher."
+  `(defun ,(intern (string-upcase (format nil "hmac-~a" hash-type))) (key input)
+     ,(format nil "HMAC-~a the input vector (octet vectors only, please) via the given key." hash-type)
+     (run-auth-hasher ',hash-type key input)))
+    
+(make-hasher md5)
 (make-hasher sha1)
 (make-hasher sha256)
 (make-hasher sha512)
-(make-hasher md5)
+
+(make-auth-hasher md5)
+(make-auth-hasher sha1)
+(make-auth-hasher sha256)
+(make-auth-hasher sha512)
 
