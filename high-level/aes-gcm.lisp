cf5e5b57fbcb3649ac83f562c150587d2ee4729b
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-05-28 01:08:14 -0700

    rename gcm.lisp

--- high-level/aes-gcm.lisp
+++ high-level/aes-gcm.lisp
@@ -6,8 +6,11 @@
 (defun encrypt-aes-gcm (key plaintext iv auth)
   "Encrypt the given plaintext with the key/IV/auth data. All arguments must be
    in binary form '(vector (unsigned-byte 8)). As the name implies, data is
-   encrypted using AES/GCM."
+   encrypted using AES/GCM. Appends the auth tag to the end of the returned
+   ciphertext."
   (declare (optimize (speed 3) (debug 0) (safety 0)))
+  (assert (or (= (length key) ne:+aes-min-key-size+)
+              (= (length key) ne:+aes-max-key-size+)))
   (with-crypto-object (ctx :gcm-aes)
     (with-static-vectors ((key-s key)
                           (plaintext-s plaintext)
@@ -31,26 +34,27 @@
 (defun decrypt-aes-gcm (key ciphertext iv auth)
   "Decrypt the given ciphertext with the key/IV/auth data. All arguments must be
    in binary form '(vector (unsigned-byte 8)). As the name implies, data is
-   decrypted using AES/GCM."
+   decrypted using AES/GCM. Assumes the auth tag is appended at the end of the
+   ciphertext."
   (declare (optimize (speed 3) (debug 0) (safety 0)))
+  (assert (or (= (length key) ne:+aes-min-key-size+)
+              (= (length key) ne:+aes-max-key-size+)))
   (with-crypto-object (ctx :gcm-aes)
     (let* ((tagsplit ne:+gcm-block-size+)
            (tag (subseq ciphertext (- (length ciphertext) tagsplit)))
-           ;; TODO: get rid of copy here, just use same array and limit length
-           ;; when calling decrypt DUHHHH
-           (ciphertext (subseq ciphertext 0 (- (length ciphertext) tagsplit))))
+           (ciphertext-length (- (length ciphertext) tagsplit)))
       (with-static-vectors ((key-s key)
                             (ciphertext-s ciphertext)
                             (iv-s iv)
                             (auth-s auth)
-                            (plaintext-s (length ciphertext))
+                            (plaintext-s ciphertext-length)
                             (tag-s ne:+gcm-block-size+))
         (ne:gcm-aes-set-key ctx (length key) (static-vector-pointer key-s))
         (ne:gcm-aes-set-iv ctx (length iv) (static-vector-pointer iv-s))
         (when (and auth (not (zerop (length auth-s))))
           (ne:gcm-aes-update ctx (length auth) (static-vector-pointer auth-s)))
         (ne:gcm-aes-decrypt ctx
-                            (length ciphertext-s)
+                            ciphertext-length
                             (static-vector-pointer plaintext-s)
                             (static-vector-pointer ciphertext-s))
         (ne:gcm-aes-digest ctx ne:+gcm-block-size+ (static-vector-pointer tag-s))
